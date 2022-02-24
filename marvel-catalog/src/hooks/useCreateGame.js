f2dec2524840646e26fa7fbcfdd960eae9efa1c0
Ed OConnor <ejoc1103@yahoo.com>
2022-02-23 21:52:13 -0500

    Debugged the issue with the usecallback hook in useCreateGame

--- marvel-catalog/src/hooks/useCreateGame.js
+++ marvel-catalog/src/hooks/useCreateGame.js
@@ -1,5 +1,4 @@
 import { useState, useEffect, useMemo, useCallback } from 'react';
-import env from 'react-dotenv';
 import md5 from 'md5';
 import axios from 'axios';
 
@@ -7,11 +6,16 @@ const useCreateGame = (reset, level) => {
   const [temp, setTemp] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [newArr, setNewArr] = useState([]);
 
   useEffect(() => {
     const date = new Date();
     const timeStamp = date.getTime();
-    const hash = md5(timeStamp + env.PRIVATE_KEY + env.PUBLIC_KEY);
+    const hash = md5(
+      timeStamp +
+        process.env.REACT_APP_PRIVATE_KEY +
+        process.env.REACT_APP_PUBLIC_KEY
+    );
     const offset = Math.floor(Math.random() * 1394);
     const fetchData = async () => {
       if (error) {
@@ -19,7 +23,7 @@ const useCreateGame = (reset, level) => {
       }
       try {
         const result = await axios.get(
-          `http://gateway.marvel.com/v1/public/characters?limit=100&offset=${offset}&ts=${timeStamp}&apikey=${env.PUBLIC_KEY}&hash=${hash}`
+          `http://gateway.marvel.com/v1/public/characters?limit=100&offset=${offset}&ts=${timeStamp}&apikey=${process.env.REACT_APP_PUBLIC_KEY}&hash=${hash}`
         );
         setLoading(false);
         setTemp(result.data.data.results);
@@ -32,30 +36,30 @@ const useCreateGame = (reset, level) => {
     fetchData();
   }, [error, reset, level]);
 
-  const newArr = useCallback(
-    temp.filter(
+  const createArr = useCallback(() => {
+    let filtered = temp.filter(
       char =>
         char.thumbnail.path !==
           'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available' &&
         char.thumbnail.path !==
           'http://i.annihil.us/u/prod/marvel/i/mg/f/60/4c002e0305708'
-    ),
-    [temp]
-  );
+    );
+    setNewArr(filtered);
+    return filtered;
+  }, [temp]);
 
-  const createCharacters = useCallback(() => {
-    let characters = [...newArr].sort(() => Math.random() - 0.05);
-    characters = newArr.slice(0, parseInt(level) / 2);
-    characters = characters.concat(characters);
-    let finalSort = [...characters].sort(() => Math.random() - 0.5);
+  useEffect(() => {
+    if (newArr.length > 0) {
+      let characters = [...newArr].sort(() => Math.random() - 0.05);
+      characters = newArr.slice(0, parseInt(level) / 2);
+      characters = characters.concat(characters);
+      let finalSort = [...characters].sort(() => Math.random() - 0.5);
 
-    return finalSort;
-  }, [level, newArr]);
+      return finalSort;
+    }
+  }, [newArr, level]);
 
-  const memoizedValue = useMemo(
-    () => createCharacters(newArr),
-    [createCharacters, newArr]
-  );
+  const memoizedValue = useMemo(() => createArr(), [createArr]);
 
   const collection = memoizedValue.map(character => ({
     id: character.id,
